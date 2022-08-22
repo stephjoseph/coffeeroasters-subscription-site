ffca317c98b611ffd38ee0bf7b01cc9252b6a9d5
Stephen Joseph Muñez <sjtmunez@gmail.com>
2022-08-22 14:53:05 +0800

    finish modal tablet view

--- components/Modal.js
+++ components/Modal.js
@@ -50,16 +50,16 @@ const Modal = () => {
       onClick={() => setIsModalOpen(false)}
     >
       <div
-        className='mt-9 flex w-[87.2%] flex-col items-center rounded-lg bg-white'
+        className='mt-9 flex w-[87.2%] flex-col items-center rounded-lg bg-white md:mt-[13.375rem] md:w-[70.31%]'
         onClick={(e) => e.stopPropagation()}
       >
-        <div className='w-full rounded-t-lg bg-dark-grey-blue'>
-          <h2 className='font-h2 w-full px-6 py-[1.75rem] text-[1.75rem] leading-[2rem] text-white'>
+        <div className='w-full rounded-t-lg bg-dark-grey-blue px-6 py-[1.75rem] md:px-14 md:py-12'>
+          <h2 className='font-h2 w-full text-[1.75rem] leading-[2rem] text-white md:text-[2.5rem] md:leading-[3rem]'>
             Order Summary
           </h2>
         </div>
-        <div className='flex w-full flex-col items-center gap-6 px-6 pt-10 pb-6'>
-          <div className='flex w-full flex-col items-center'>
+        <div className='flex w-full flex-col items-center gap-6 px-6 pt-10 pb-6 md:gap-12 md:p-14'>
+          <div className='flex w-full flex-col items-center md:gap-2'>
             <p className='font-h2 text-[1.5rem] leading-10 text-grey'>
               “I drink my coffee as{' '}
               <span className='text-dark-cyan'>{state.preferences}</span>, with
@@ -75,19 +75,25 @@ const Modal = () => {
               <span className='text-dark-cyan'>{state.deliveries}</span>
               .”
             </p>
-            <p className='font-body text-[0.938rem] leading-[1.563rem] text-dark-grey-blue/80'>
+            <p className='font-body md:font-body text-[0.938rem] leading-[1.563rem] text-dark-grey-blue/80'>
               Is this correct? You can proceed to checkout or go back to plan
               selection if something is off. Subscription discount codes can
               also be redeemed at the checkout.{' '}
             </p>
           </div>
-          <div className='w-full'>
+          <div className='w-full md:flex md:flex-row md:items-center md:gap-[0.813rem]'>
+            <div className='font-checkout hidden w-[46.4%] md:block'>
+              ${pricePerMonth} / mo
+            </div>
             <button
               type='button'
-              className='btn w-full'
+              className='btn w-full md:w-[50.70%]'
               onClick={handleCheckout}
             >
-              Checkout - ${pricePerMonth} / mo
+              <div>
+                Checkout{' '}
+                <span className='md:hidden'> - ${pricePerMonth} / mo</span>
+              </div>
             </button>
           </div>
         </div>
